f9277a8b1143c3120e6f556d6e4aa925076adcbf
Mr Cloud <127306466+whoavesh@users.noreply.github.com>
2025-04-03 17:34:46 +0100

    update docker push

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,7 +1,7 @@
 def call(String credId, String imageName){
      withCredentials([usernamePassword(credentialsId:"${credId}",passwordVariable:"dockerHubPass",usernameVariable:"dockerHubUser")]){
                     sh "docker login -u ${env.dockerHubUser} -p ${env.dockerHubPass}"
-                    sh "docker tag "${imageName}" ${env.dockerHubUser}/"${imageName}":latest"
-                    sh "docker push ${env.dockerHubUser}/"${imageName}":latest" 
+                    sh "docker tag "${imageName} ${env.dockerHubUser}/${imageName}":latest"
+                    sh "docker push ${env.dockerHubUser}/${imageName}":latest" 
                 }
 }
